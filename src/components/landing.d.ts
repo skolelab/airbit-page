d7fa9d4c3a652711b148ee79999aff1a38498daa
Erling Devold <erling@devold.tech>
2021-07-19 13:55:44 +0200

    modified:   build/index.bundle.js 	modified:   src/components/landing.d.ts 	modified:   webpack.config.js

--- src/components/landing.d.ts
+++ src/components/landing.d.ts
@@ -1,6 +1,8 @@
-/// <reference types="react" />
+import { Dispatch, SetStateAction } from "react";
 declare type LandingProps = {
     language: boolean;
+    wiki: boolean;
+    setWiki: Dispatch<SetStateAction<boolean>>;
 };
-declare const Landing: ({ language }: LandingProps) => JSX.Element;
+declare const Landing: ({ language, wiki, setWiki }: LandingProps) => JSX.Element;
 export default Landing;
